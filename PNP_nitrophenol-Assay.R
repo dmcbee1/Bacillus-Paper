6f421a768dd298705a13f841da77c6168c73c5f4
dmcbee1 <147542915+dmcbee1@users.noreply.github.com>
2024-01-11 12:15:37 -0500

    Add files via upload

--- PNP_nitrophenol-Assay.R
+++ PNP_nitrophenol-Assay.R
@@ -1,244 +1,190 @@
+#####Inital Data Workup and Libary Loading#####
+
 ##Load in Libararies
 library(readxl)
 library(tidyverse)
 library(broom)
 library(rstatix)
 library(ggpubr)
-library(RColorBrewer)
-library(ggsci)
+library(openxlsx)
+library(viridis)
 
-#Funcitons
-zero_data <- function(data) {
-  ave <- data %>%
-    slice(1) %>%
-    summarise(across(starts_with("w"), mean, na.rm = TRUE)) %>%
-    pull()
-  
-  result <- data %>%
-    mutate(across(starts_with("w"), ~ . - ave))%>%
-    mutate(across(where(is.numeric), ~ replace(., . < 0, 0)))
+##Enter in working directory and Dataset location
+dataset1 <-'/Users/dmcbee/Library/CloudStorage/OneDrive-UniversityofTennessee/Dillon/1 Current Projects/1 Bacterial IPPDMAPP Paper/RF-1_PNP-assay.xlsx'
+sheet_name <- 'Plate 1 - Sheet1'
+names1 <- '/Users/dmcbee/Library/CloudStorage/OneDrive-UniversityofTennessee/Dillon/1 Current Projects/Rudolph Colab/PNP_AssayRC_1_P1_2023-12-08/Name_Template_PNP.xlsx'
+new_folder_name <- 'RC_1_P1_'
+base_path <- "/Users/dmcbee/Library/CloudStorage/OneDrive-UniversityofTennessee/Dillon/1 Current Projects/Rudolph Colab/PNP_Assay"
+
+# Specify the number of experimental conditions
+num_conditions <- 9 #Update this number based on your experiment
+
+# Create the folder name with the current date
+folder_name <- paste0(base_path, new_folder_name, Sys.Date())
+# Create the new directory
+if (!dir.exists(folder_name)) {
+  dir.create(folder_name)
 }
 
-##Import Data
-raw1 <- read_excel('', 
-                   col_names = TRUE,
-                   skip = 1)# File path of data
-setwd('') #Set working directory to send files
+# Set the new directory as the working directory
+setwd(folder_name)
 
-##Name Triplicates
-##    Use shift+command+c to # out any unused code
-triplicate_names <- as_labeller(c(
-  'T1'="PNP",
-  'T2'="Ac-PNP",
-  'T3'="Prop-PNP",
-  'T4'="But-PNP",
-  'T5'='Isobut-PNP',
-  'T6'='Piv-PNP',
-  'T7'= 'Pent-PNP',
-  'T8'= 'Hex-PNP',
-  'T9'= 'Hept-PNP',
-  'T10'= 'Bz-PNP',
-  'T11'= 'Veh'))
-  # 'T12'= '1 µM Sat-Hex'))
+#Copy Raw data into new folder
+file.copy(from = dataset1, to = folder_name)
+file.copy(from = names1, to = folder_name)
 
-#Chose colors for the graph
-palette_names <- 'Paired'
-#Get colors from chosen pallet
-colors <- brewer.pal(palette_names, n=12)
-
-##Remove Temp
-raw2 <- raw1 %>%
-  # select(-'T° 600') %>%
-  mutate(Time = seq(0, (n() - 1) * 1/15, 1/15))%>%
+#Import Data
+treatment1 <- read_excel(dataset1,
+                         sheet = sheet_name)%>%
+  mutate(Time = seq(0, (n() - 1) * 1/30, 1/30))%>%
+  select(-'T° Read 2:400')%>%
   drop_na()
 
-##Row A
-T1 <- raw2 %>%
-  select(Time, A1:A3)%>%
-  add_column('T1')%>%
-  rename('time' = 'Time', 
-         'w1' = 'A1',
-         'w2' = 'A2',
-         'w3' = 'A3',
-         'group' =  "\"T1\"")%>%
-  zero_data()
+# Calculate the number of columns to keep (3 columns per condition + 1 for the Time column)
+num_columns_to_keep <- num_conditions * 3 + 1
 
-T2 <- raw2 %>%
-  select(Time, A4:A6)%>%
-  add_column('T2')%>%
-  rename('time' = 'Time', 
-         'w1' = 'A4',
-         'w2' = 'A5',
-         'w3' = 'A6',
-         'group' =  "\"T2\"")%>%
-  zero_data()
+# Select the necessary columns (Time column + the first 'num_columns_to_keep - 1' columns of data)
+treatment1 <- treatment1 %>%
+  select(1:num_columns_to_keep) %>%
+  drop_na()
 
-T3 <- raw2 %>%
-  select(Time, A7:A9)%>%
-  add_column('T3')%>%
-  rename('time' = 'Time', 
-         'w1' = 'A7',
-         'w2' = 'A8',
-         'w3' = 'A9',
-         'group' =  "\"T3\"")%>%
-  zero_data()
+# Replace 'OVRFLW' with NA
+treatment1[treatment1 == 'OVRFLW'] <- NA
 
-T4 <- raw2 %>%
-  select(Time, A10:A12)%>%
-  add_column('T4')%>%
-  rename('time' = 'Time', 
-         'w1' = 'A10',
-         'w2' = 'A11',
-         'w3' = 'A12',
-         'group' =  "\"T4\"")%>%
-  zero_data()
-
-##Row_B
-T5 <- raw2 %>%
-  select(Time, B1:B3)%>%
-  add_column('T5')%>%
-  rename('time' = 'Time',
-         'w1' = 'B1',
-         'w2' = 'B2',
-         'w3' = 'B3',
-         'group' =  "\"T5\"")%>%
-  zero_data()
-
-T6 <- raw2 %>%
-  select(Time, B4:B6)%>%
-  add_column('T6')%>%
-  rename('time' = 'Time',
-         'w1' = 'B4',
-         'w2' = 'B5',
-         'w3' = 'B6',
-         'group' =  "\"T6\"")%>%
-  zero_data()
-
-T7 <- raw2 %>%
-  select(Time, B7:B9)%>%
-  add_column('T7')%>%
-  rename('time' = 'Time',
-         'w1' = 'B7',
-         'w2' = 'B8',
-         'w3' = 'B9',
-         'group' =  "\"T7\"")%>%
-  zero_data()
-
-T8 <- raw2 %>%
-  select(Time, B10:B12)%>%
-  add_column('T8')%>%
-  rename('time' = 'Time',
-         'w1' = 'B10',
-         'w2' = 'B11',
-         'w3' = 'B12',
-         'group' =  "\"T8\"")%>%
-  zero_data()
-
-##Row C
-
-T9 <- raw2 %>%
-  select(Time, C1:C3)%>%
-  add_column('T9')%>%
-  rename('time' = 'Time',
-         'w1' = 'C1',
-         'w2' = 'C2',
-         'w3' = 'C3',
-         'group' =  "\"T9\"")%>%
-  zero_data()
-
-T10 <- raw2 %>%
-  select(Time, C4:C6)%>%
-  add_column('T10')%>%
-  rename('time' = 'Time',
-         'w1' = 'C4',
-         'w2' = 'C5',
-         'w3' = 'C6',
-         'group' =  "\"T10\"")%>%
-  zero_data()
-
-T11 <- raw2 %>%
-  select(Time, C7:C9)%>%
-  add_column('T11')%>%
-  rename('time' = 'Time',
-         'w1' = 'C7',
-         'w2' = 'C8',
-         'w3' = 'C9',
-         'group' =  "\"T11\"")%>%
-  zero_data()
-
-T12 <- raw2 %>%
-  select(Time, C10:C12)%>%
-  add_column('T12')%>%
-  rename('time' = 'Time',
-         'w1' = 'C10',
-         'w2' = 'C11',
-         'w3' = 'C12',
-         'group' =  "\"T12\"")%>%
-  zero_data()
-
-##Combine data and add in mean, stdev
-Comb_Dat <- bind_rows(T2,T3,T4,T5,T6,T7,T8,T9,T10,T11)%>%
-  group_by(time,group)%>%
-  mutate(mean = mean(c(w1,w2,w3)))%>%
-  mutate(stdev = sd(c(w1,w2,w3)))%>%
-  mutate(ODmax = mean+stdev)%>%
-  mutate(ODmin = mean-stdev)%>%
-  filter(time <=10)
-
-##Scatter plot of growth curve
-ggplot()+
-  geom_point(Comb_Dat, mapping = aes(x=time, y= w1, color = group), size = 0.1,show.legend = FALSE)+
-  geom_point(Comb_Dat, mapping = aes(x=time, y= w2, color = group), size = 0.1,show.legend = FALSE)+
-  geom_point(Comb_Dat, mapping = aes(x=time, y= w3, color = group), size = 0.1,show.legend = FALSE)+
-  geom_smooth(Comb_Dat, mapping = aes(x=time, y= mean, color = group),method = 'loess',show.legend = TRUE)+
-  labs(x='Time (h)', y='OD 400 nm', color='Group', aes(size=10))+
-  scale_color_manual(
-    values = colors,
-    breaks = c('T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9', 'T10', 'T11', 'T12'),
-    labels = triplicate_names
-  )+
-  scale_y_continuous(limits = c(0, 1.3))+
-  # facet_wrap(~factor(group,c('T1','T2','T3','T4','T5','T6','T7','T8','T9','T10'
-  #                             ,'T11', 'T12')),
-  #            labeller = triplicate_names)+
-  theme_classic()+
-  theme(legend.position = 'bottom') 
-
-ggsave('DPM-F-53_PNP-LiveCellAssay_ecoli_no-Control_16Aug2023.pdf', width = 6, height = 4)
-ggsave('DPM-F-53_PNP-LiveCellAssay_ecoli_no-Control_16Aug2023.png', width = 6, height = 4)
+# Normalization function: Subtracts the first measurement from all values in a vector
+normalize_curve <- function(data_vector) {
+  # Ensure that the data vector is numeric
+  data_vector <- as.numeric(data_vector)
   
-##Graph of OD600 vs Conc at Defined Time####
-
-conc_labels <- list(
-  'T1'= 0,
-  'T2'= 0,
-  'T3'= 75,
-  'T4'= 50,
-  'T5'= 40,
-  'T6'= 30,
-  'T7'= 20,
-  'T8'= 10,
-  'T9'= 5,
-  'T10'= 1,
-  'T11'= 0.5,
-  'T12'= 0.1)
-
-timepoint10h <- Comb_Dat%>%
-  filter(time == '8')%>%
-  ungroup()%>%
-  add_column(conc = conc_labels)%>%
-  mutate(concentration = as.numeric(conc_labels))%>%
-  select(-conc)%>%
-  arrange(concentration)%>%
-  slice(2:12)
-
-ggplot(timepoint10h, mapping = aes(x=concentration, y=mean, label = round(mean, digits = 2)))+
-  geom_point(show.legend = FALSE)+
-  # geom_label(hjust = 0, nudge_y = -0.05, nudge_x = 0.5)+
-  geom_errorbar(aes(ymin=ODmin, ymax=ODmax))+
-  geom_smooth(color='black',method = 'lm',show.legend = FALSE)+
-  labs(x='Concentration (µM)', y='OD 600 nm @ 8 h', aes(size=5))+
-  theme_classic()
+  # Use the first non-NA value for normalization
+  first_val <- data_vector[!is.na(data_vector)][1]
   
-ggsave('', width = 6, height = 4)
-ggsave('', width = 6, height = 4)
+  # If the entire vector is NA, return it as is
+  if (is.na(first_val)) {
+    return(data_vector)
+  }
+  
+  # Normalize and replace negative values with 0
+  normalized_data <- data_vector - first_val
+  return(pmax(normalized_data, 0, na.rm = TRUE))
+}
+
+# Apply normalization to each growth curve column
+for (i in 2:ncol(treatment1)) {
+  treatment1[[i]] <- normalize_curve(treatment1[[i]])
+}
+
+# Determine the number of groups (assuming first column is 'Time' and the data are in triplicates)
+num_groups <- (ncol(treatment1) - 1) / 3
+
+# Generate custom group names dynamically
+letters_part <- rep(LETTERS[1:ceiling(num_groups / 4)], each = 4)
+numbers_part <- rep(1:4, times = ceiling(num_groups / 4))
+custom_group_names <- paste0(letters_part[1:num_groups], numbers_part[1:num_groups])
+
+new_labels_df <- read_excel(names1)
+new_labels <- setNames(new_labels_df[[1]], custom_group_names)
+custom_labeller <- setNames(as.character(new_labels_df[[1]]), custom_group_names)
+
+# Create an empty list to store data frames
+result_list <- list()
+
+# Initialize a counter
+counter <- 1
+
+# Loop over the range of column indices (assuming your data starts at 2nd column)
+for (i in seq(2, ncol(treatment1), by = 3)) {
+  
+  # Generate group name dynamically
+  group_name <- colnames(treatment1)[i]
+  
+  # Extract just the part before the number (e.g., "A" from "A1")
+  base_group <- substr(group_name, 1, 1)
+  
+  # Generate a new data frame with the selected columns and new names
+  temp_df <- treatment1 %>%
+    select(Time, i:(i+2)) %>%
+    add_column(group = custom_group_names[counter]) %>%  # Replace 'base_group' with custom group name
+    rename(time = "Time",
+           w1 = colnames(treatment1)[i],
+           w2 = colnames(treatment1)[i+1],
+           w3 = colnames(treatment1)[i+2])
+  
+  # Add the newly created data frame to the list
+  result_list[[group_name]] <- temp_df
+  
+  # Increment the counter
+  counter <- counter + 1
+}
+
+
+# Now result_list is a list of data frames, each processed as in your original code
+
+# Combine all data frames into one
+combined_df <- dplyr::bind_rows(result_list)
+
+
+# Reshape to long format
+long_df <- tidyr::pivot_longer(
+  filtered_df,
+  c(w1, w2, w3),
+  names_to = "replicate",
+  values_to = "value")
+
+your_color_values <- c('#1f77b4',
+                      '#ff7f0e',
+                      '#2ca02c',
+                      '#d62728',
+                      '#9467bd',
+                      '#8c564b',
+                      '#e377c2',
+                      '#7f7f7f',
+                      '#bcbd22')
+
+# Calculate central point (e.g., median) for each group at each time
+central_points <- long_df %>%
+  group_by(group, time) %>%
+  summarise(central_value = median(value))
+
+
+ggplot(long_df, aes(x = time, y = value, color = group)) +
+  geom_point(size = 0.1, alpha = 0.6) +
+  geom_smooth(data = central_points, aes(y = central_value, color = group), method = "loess", se = FALSE, size = 0.5, span = 1) +  # Smoothing line
+  labs(x = "Time (h)", y = "Absorbance 400 nm", color = "Group") +
+  theme_classic() +
+  scale_y_continuous(limits = c(0, 1), breaks = c(0.0, 0.5, 1.0)) +
+  scale_x_continuous(limits = c(0, 1), breaks = c(0, 0.5, 1)) +
+  theme(axis.text.x = element_text(size = 8),
+        axis.text.y = element_text(size = 8),
+        axis.title.x = element_text(size = 10),
+        axis.title.y = element_text(size = 10),
+        legend.position = "bottom",
+        legend.text = element_text(size = 7),  # Reduce text size
+        legend.key.size = unit(0.3, "cm"),  # Reduce key size
+        legend.spacing.x = unit(0.1, "cm"),  # Adjust horizontal spacing
+        legend.spacing.y = unit(0.2, "cm")) +  # Adjust vertical spacing
+  scale_color_manual(values = your_color_values, labels = custom_labeller)  # Custom color scale and labels
+
+
+ggsave(paste0("PNP_OD500_Combined_", new_folder_name, ".png"), width=3, height = 2.5)
+ggsave(paste0("PNP_OD500_Combined_", new_folder_name, ".pdf"), width=3, height = 2.5)
+
+
+## Facuet Graph
+ggplot(long_df, aes(x = time, y = value, color = group)) +
+  geom_point(size = 1) +
+  geom_smooth(data = central_points, aes(y = central_value), method = "gam", se = FALSE, size = 1) +  # Smoothing line
+  labs(x = "Time (h)", y = "OD 400 nm", color = "Group") +
+  facet_wrap(~ group, labeller = as_labeller(custom_labeller)) +  # updated labeller
+  theme_classic() +
+  scale_y_continuous(limits = c(0, 1), breaks = c(0.0, 0.5, 1.0)) +
+  scale_x_continuous(limits = c(0, 1), breaks = c(0, 0.5, 1)) +
+  theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 16),
+        legend.position = "none")  # y-axis title
+
+ggsave(paste0("PNP_OD500_Facet_", new_folder_name, ".png"), width=10, height = 7)
+ggsave(paste0("PNP_OD500_Facet_", new_folder_name, ".pdf"), width=10, height = 7)
+
